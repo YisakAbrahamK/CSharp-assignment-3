b29e7f53d26823d7c778a166713e310d7974d26c
YisakAbrahamK <107458880+YisakAbrahamK@users.noreply.github.com>
2022-06-29 13:42:44 +0300

    Added unit of measurement in Program.cs

--- Shape Project/Shape Project/Program.cs
+++ Shape Project/Shape Project/Program.cs
@@ -8,17 +8,17 @@
             Rectangle rectangle = new ();
 
             Console.WriteLine("-------------------------------------------------------------------");
-            Console.Write("Enter the radious of the circle: ");
+            Console.Write("Enter the radious(m) of the circle: ");
             circle.radious = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("The Area of the circle is: " + circle.calculateArea());
-            Console.WriteLine("The Parameter of the circle is: " + circle.calculateParameter());
+            Console.WriteLine("The Area of the circle is: " + circle.calculateArea() + " meter square");
+            Console.WriteLine("The Parameter of the circle is: " + circle.calculateParameter() + " meter");
             Console.WriteLine("-------------------------------------------------------------------");
-            Console.Write("Enter the Length of the rectangle: ");
+            Console.Write("Enter the Length(m) of the rectangle: ");
             rectangle.Length = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter the width of the rectangle: ");
+            Console.Write("Enter the width(m) of the rectangle: ");
             rectangle.Width = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("The Area of the rectangle is: " + rectangle.calculateArea());
-            Console.WriteLine("The Parameter of the rectangle is: " + rectangle.calculateParameter());
+            Console.WriteLine("The Area of the rectangle is: " + rectangle.calculateArea() + " meter square");
+            Console.WriteLine("The Parameter of the rectangle is: " + rectangle.calculateParameter() + " meter");
             Console.WriteLine("-------------------------------------------------------------------");
 
         }
